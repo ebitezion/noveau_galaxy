ba6fd8dc646e94253645204cb3315d5e7de3b7eb
adeoluwa <akanbiadenugba699@gmail.com>
2023-12-18 15:22:55 +0100

    cleared out old sql files

--- migrations/sql/4-create-account-auth-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS accounts_auth (
-`id` int NOT NULL AUTO_INCREMENT,
-`accountNumber` char(36) NOT NULL, 
-`password` varchar(255) NOT NULL,
-`timestamp` int NOT NULL,
-PRIMARY KEY (`id`)
-);
-
